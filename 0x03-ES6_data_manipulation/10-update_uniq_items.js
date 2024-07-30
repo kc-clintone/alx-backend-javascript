eb6702e31d491117605bfa9047f9f287ff61dabc
kc-clintone <kayseeclintone@gmail.com>
2024-07-30 13:49:26 +0000

    task 19

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [k, v] of map) {
+      if (v === 1) {
+        map.set(k, 100);
+      }
+    }
+    return map;
+  }
+  throw new Error('Cannot process');
+}
