3a14f93c19087f590905c166969271496e148f25
kc-clintone <kayseeclintone@gmail.com>
2024-07-31 20:44:19 +0000

    corrections

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw Error('Position outside range');
   }
-  const bfr = new BfrArray(length);
-  const intArr = new IntArr(bfr, 0, length);
+  const bfr = new ArrayBuffer(length);
+  const intArr = new Int8(bfr, 0, length);
   intArr.set([value], position);
   return new DataView(bfr);
 }
