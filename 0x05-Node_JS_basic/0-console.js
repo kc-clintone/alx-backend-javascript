6d0e6e28ccd3d2b5ea8ec49750c388cb0fafae60
kc-clintone <kayseeclintone@gmail.com>
2024-09-20 07:00:57 +0000

    Task 0: 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * displayMessage
+ *
+ * Prints the provided string argument to STDOUT using console.log.
+ * @author - Clinton Otieno, www.github.com/kc-clintone
+ * @param {string} message - The message to be printed to the console.
+ */
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
