31f9f702ee4b8b230bb1f5bbcbc4696200249630
kc-clintone <kayseeclintone@gmail.com>
2024-09-21 13:03:47 +0000

    Corrections

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -11,39 +11,45 @@ const fs = require('fs');
  * @throws Will throw an error if the file cannot be read.
  */
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
+  try {
+    const data = fs.readFileSync(path, 'utf8');
 
-        const lines = data.trim().split('\n').filter(line => line !== '');
+    const lines = data
+      .trim()
+      .split('\n')
+      .filter((line) => line !== '');
 
-        const header = lines[0].split(',');
-        const studentData = lines.slice(1);
+    const header = lines[0].split(','); // eslint-disable-line no-unused-vars
+    const studentData = lines.slice(1);
 
-        if (studentData.length === 0) {
-            console.log('No students found in the database.');
-            return;
-        }
-
-        console.log(`Number of students: ${studentData.length}`);
-
-        const fieldGroups = {};
-
-        studentData.forEach((line) => {
-            const [firstname, lastname, age, field] = line.split(',');
-
-            if (field in fieldGroups) {
-                fieldGroups[field].push(firstname);
-            } else {
-                fieldGroups[field] = [firstname];
-            }
-        });
-
-        for (const [field, students] of Object.entries(fieldGroups)) {
-            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+    if (studentData.length === 0) {
+      console.log('No students found in the database.');
+      return;
     }
+
+    console.log(`Number of students: ${studentData.length}`);
+
+    const fieldGroups = {};
+
+    studentData.forEach((line) => {
+      /*eslint-disable-line no-unused-vars*/
+      const [firstname, lastname, age, field] = line.split(',');
+      /*eslint-disable-line no-unused-vars*/
+      if (field in fieldGroups) {
+        fieldGroups[field].push(firstname);
+      } else {
+        fieldGroups[field] = [firstname];
+      }
+    });
+
+    for (const [field, students] of Object.entries(fieldGroups)) {
+      console.log(
+        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
