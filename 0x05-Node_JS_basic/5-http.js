fc7d9c456b30c21896c8de7b79d1f2e41b2cc8c7
kc-clintone <kayseeclintone@gmail.com>
2024-09-21 14:14:33 +0000

    Corrections

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,59 +9,52 @@ const database_file = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Clinton Otieno - www.github.com/kc-clintone
+ * @author Clinton Otieno
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
-    reject(new Error('Cannot load the database'));
+    return reject(new Error('Cannot load the database'));
   }
-  if (dataPath) {
-    fs.readFile(dataPath, (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const reports = [];
-        const fileLines = data.toString('utf-8').trim().split('\n');
-        const studGroups = {};
-        const dbfnames = fileLines[0].split(',');
-        const studNames = dbfnames.slice(
-          0,
-          dbfnames.length - 1,
-        );
 
-        for (const line of fileLines.slice(1)) {
-          const studRecord = line.split(',');
-          const studPropValues = studRecord.slice(
-            0,
-            studRecord.length - 1,
-          );
-          const myfield = studRecord[studRecord.length - 1];
-          if (!Object.keys(studGroups).includes(myfield)) {
-            studGroups[myfield] = [];
-          }
-          const studEntries = studPropNames.map((propName, index) => [
-            propName,
-            studPropValues[index],
-          ]);
-          studGroups[myfield].push(Object.fromEntries(studEntries));
-        }
+  fs.readFile(dataPath, (error, data) => {
+    if (error) {
+      return reject(new Error('Cannot load the database'));
+    }
 
-        const totalStuds = Object.values(studGroups).reduce(
-          (last, current) => (last || []).length + current.length,
-        );
-        reports.push(`Number of students: ${totalStuds}`);
-        for (const [myfield, group] of Object.entries(studGroups)) {
-          reports.push([
-            `Number of students in ${myfield}: ${group.length}.`,
-            'List:',
-            group.map((student) => student.firstname).join(', '),
-          ].join(' '));
-        }
-        resolve(reports.join('\n'));
+    const reports = [];
+    const fileLines = data.toString('utf-8').trim().split('\n');
+    const studGroups = {};
+    const dbfnames = fileLines[0].split(',');
+    const studNames = dbfnames.slice(0, dbfnames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studRecord = line.split(',');
+      const studPropValues = studRecord.slice(0, studRecord.length - 1);
+      const myfield = studRecord[studRecord.length - 1];
+      if (!Object.keys(studGroups).includes(myfield)) {
+        studGroups[myfield] = [];
       }
-    });
-  }
+      const studEntries = studNames.map((propName, index) => [
+        propName,
+        studPropValues[index],
+      ]);
+      studGroups[myfield].push(Object.fromEntries(studEntries));
+    }
+
+    const totalStuds = Object.values(studGroups).reduce(
+      (total, group) => total + group.length,
+      0
+    );
+    reports.push(`Number of students: ${totalStuds}`);
+    for (const [myfield, group] of Object.entries(studGroups)) {
+      reports.push([
+        `Number of students in ${myfield}: ${group.length}.`,
+        'List:',
+        group.map((student) => student.firstname).join(', '),
+      ].join(' '));
+    }
+    resolve(reports.join('\n'));
+  });
 });
 
 const handleServerRoutes = [
@@ -71,9 +64,10 @@ const handleServerRoutes = [
       const myResponse = 'Hello Holberton School!';
 
       res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Content-Length', myResponse.length);
+      res.setHeader('Content-Length', Buffer.byteLength(myResponse));
       res.statusCode = 200;
       res.write(Buffer.from(myResponse));
+      res.end();
     },
   },
   {
@@ -86,17 +80,19 @@ const handleServerRoutes = [
           resps.push(report);
           const myResponse = resps.join('\n');
           res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', myResponse.length);
+          res.setHeader('Content-Length', Buffer.byteLength(myResponse));
           res.statusCode = 200;
           res.write(Buffer.from(myResponse));
+          res.end();
         })
         .catch((error) => {
           resps.push(error instanceof Error ? error.message : error.toString());
           const myResponse = resps.join('\n');
           res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', myResponse.length);
+          res.setHeader('Content-Length', Buffer.byteLength(myResponse));
           res.statusCode = 200;
           res.write(Buffer.from(myResponse));
+          res.end();
         });
     },
   },
@@ -106,9 +102,16 @@ app.on('request', (req, res) => {
   for (const handleRoutes of handleServerRoutes) {
     if (handleRoutes.route === req.url) {
       handleRoutes.handler(req, res);
-      break;
+      return;
     }
   }
+
+  res.statusCode = 404;
+  const notFoundMessage = '404 Not Found';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', Buffer.byteLength(notFoundMessage));
+  res.write(Buffer.from(notFoundMessage));
+  res.end();
 });
 
 app.listen(port, host, () => {
