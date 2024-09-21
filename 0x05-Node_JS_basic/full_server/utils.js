1703770e609a4ea2e6f8ba5d4bbbca7d906f617f
kc-clintone <kayseeclintone@gmail.com>
2024-09-21 15:36:55 +0000

    Corrections

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export const readDatabase = (filePath) => {
+export function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
