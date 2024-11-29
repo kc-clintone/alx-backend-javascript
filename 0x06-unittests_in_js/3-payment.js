6f848e0923156a2c845d4b76164583011a3519bc
kc-clintone <kayseeclintone@gmail.com>
2024-11-29 11:57:06 +0300

    corrections

--- 0x06-unittests_in_js/3-payment.js
+++
@@ -1,8 +0,0 @@
-const Utils = require('./utils');
-
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const sumCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${sumCost}`);
-};
-
-module.exports = sendPaymentRequestToApi;
